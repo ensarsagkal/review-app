2457a28ef48929a3e9fe0100b3b4ac4f67732de6
ensar <ensarsagkal@gmail.com>
2023-12-07 13:22:59 +0300

    react done

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,6 +5,22 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const[index,setIndex]=useState(0)
   const {name,job,image,text}=data[index]
+  const handleNext = ()=>{
+    index<data.length-1 ? setIndex(index+1): setIndex(0)
+  }
+  const handlePrev=()=>{
+    index ===0 ? setIndex(data.length-1):setIndex(index-1)
+  }
+  const handleRandom = ()=>{
+
+    let random = Math.floor(Math.random() *( data.length -1))
+    console.log(random);
+    if(random ===index){
+        random = index +1
+        
+    }
+    setIndex(random)
+  }
   return (
     <div className='review'>
       <div className="img-div">
@@ -22,7 +38,7 @@ const Review = () => {
           <FaChevronRight/>
         </button>
       </div>
-      <button className='random-btn'onClick={handleRandom}>Suprise Me</button>
+      <button className='random-btn' onClick={handleRandom}>Suprise Me</button>
     </div>
   )
 }
