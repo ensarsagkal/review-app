e8574b24c74d3b727acc20fe821f153ed500b892
ensar <ensarsagkal@gmail.com>
2023-12-07 12:49:46 +0300

    card added

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from "../data"
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
+  const[index,setIndex]=useState(0)
+  const {name,job,image,text}=data[index]
   return (
-    <div>Reviews</div>
+    <div className='review'>
+      <div className="img-div">
+      <img src={image} alt={name} />
+      <span className='quote-icon'><FaQuoteRight/> </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className='prev-btn' onClick={handlePrev}>
+          <FaChevronLeft/>
+        </button>
+        <button className='next-btn'onClick={handleNext}>
+          <FaChevronRight/>
+        </button>
+      </div>
+      <button className='random-btn'onClick={handleRandom}>Suprise Me</button>
+    </div>
   )
 }
 
